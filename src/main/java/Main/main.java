3a1f1807de1b784aa55867e0e8f1d78faad23dae
Vikrot Veresov <mymoonneey@yandex.ru>
2024-03-16 21:36:59 +0300

    hw second semi

--- src/main/java/Main/main.java
+++ src/main/java/Main/main.java
@@ -13,34 +13,7 @@ public class main {
         secondSemi_PrintTeam();
     }
 
-//    public static void firstSemi() {
-////    Проанализировать персонажей
-////    "Крестьянин, Разбойник, Снайпер, Колдун, Копейщик, Арбалетчик, Монах".
-////    Для каждого определить 8 полей данных(здоровье, сила итд)
-////    3-4 поля поведения(методов атаковать, вылечить итд).
-////    Создать абстрактный класс и иерархию наследников.
-////    Расположить классы в пакет так, чтобы в основной программе небыло видно их полей.
-////    В не абстрактных классах переопределить метод toString()
-////    так чтобы он возвращал название класса или имя.
-////    Создать в основной программе по одному обьекту каждого
-////    не абстрактного класса и вывести в консоль его имя.
-//
-//        peasant peasant = new peasant("Крестьянин");
-//        crossbowman crossbowman = new crossbowman("Арбалетчик");
-//        monk monk = new monk("Монах");
-//        robber robber = new robber("Разбойник");
-//        sniper sniper = new sniper("Снайпер");
-//        spearman spearman = new spearman("Копейщик");
-//        wizard wizard = new wizard("Колдун");
-//        System.out.println(peasant);
-//        System.out.println(crossbowman);
-//        System.out.println(monk);
-//        System.out.println(robber);
-//        System.out.println(sniper);
-//        System.out.println(spearman);
-//        System.out.println(wizard);
-//
-//    }
+
 
     public static void secondSemi_CreateTeam(ArrayList<Person> team, int number, int start) {
 //        Создать две команды по 10 человек.
